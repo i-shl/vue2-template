cd533ce3967205e1d0c96a83fd1462380db19c36
申虎林 <i-shl@qq.com>
2022-12-16 13:41:41 +0800

    跨域

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,3 @@
 export const baseURL = "http://kumanxuan1.f3322.net:8001"; //测试域名
+// export const baseURL = '/api' //本地跨域域名
 // export const baseURL = 'http://kumanxuan1.f3322.net:8001' //正式域名
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,21 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  // devServer: {
+  //   host: "localhost",
+  //   port: 9020, // 指定端口
+  //   open: true, //设置true为打开默认浏览器
+  //   hot: true, //热更新
+  //   proxy: {
+  //     "/api": {
+  //       target: "", //要跨域的域名
+  //       ws: true, // 是否启用websockets
+  //       changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求
+  //       pathRewrite: {
+  //         //重写匹配的字段，如果不需要在请求路径上，重写为""
+  //         "^/api": "/api",
+  //       },
+  //     },
+  //   },
+  // },
 })
